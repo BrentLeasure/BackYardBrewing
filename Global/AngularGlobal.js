bf15f522bf3cb8584d4553e64531fdf9272b090d
Brent Leasure <brenton.leasure@hotmail.com>
2015-09-24 16:46:12 -0600

    Not going to save these

--- Global/AngularGlobal.js
+++ Global/AngularGlobal.js
@@ -10,9 +10,9 @@ angular.module("indexModule")
 	.controller("headerController", ["$scope", "$window", "$interval", "mainFactory", function($scope, $window, $interval, mainFactory){
 		$scope.state = false;
 		$interval(function(){
-			if($window.pageYOffset >= 150){
+			if($window.pageYOffset >= 190){
 				$scope.state = true;
-			}else if($window.pageYOffset < 150){
+			}else if($window.pageYOffset < 190){
 				$scope.state = false;	
 			}
 		}, 100);
