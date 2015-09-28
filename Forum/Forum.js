f53e49b16b59905d43aa8eed301aadbef6688aaa
Brent Leasure <brenton.leasure@hotmail.com>
2015-09-28 14:32:06 -0600

    Progress made on forums layout

---
+++ Forum/Forum.js
@@ -0,0 +1,4 @@
+angular.module("indexModule")
+	.controller("forumController", ["$scope", function(){
+
+	}]);
--- home/home.js
+++ home/home.js
@@ -1,5 +1,22 @@
+var eventMap;
+function initMap() {
+	var centerOfMap ={lat: 39.244785, lng: -105.511852};
+
+	eventMap = new google.maps.Map(document.getElementById('eventMap'), {
+		center: centerOfMap,
+		zoom: 7,
+		scrollwheel: false
+  	});
+
+ 	var marker = new google.maps.Marker({
+		position: centerOfMap,
+		map: eventMap,
+		title: 'Hello World!'
+	});
+}
+
 angular.module("indexModule")
-	.controller("mainController", ["$scope", function(){
+	.controller("homePageController", ["$scope", function(){
 
 
 	}]);
