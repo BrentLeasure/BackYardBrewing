cd0661c43a3dedcfa5efebf070184687de19f025
Brenton Leasure <brenton.leasure@hotmail.com>
2015-10-08 23:49:48 -0600

    setting up the web server

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ var server = express();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "./public"));
+
+server.get("/", function(req, res){
+	res.sendFile("/Home.html", {root: "./Home"})
+});
+
+server.get("/forums", function(req, res){
+	res.sendFile("/Forum.html", {root: "./Forum"})
+});
+server.get("/recipesubmission", function(req, res){
+	res.sendFile("/RecipeSubmission.html", {root: "./Recipe-Submission"})
+});
+server.get("/recipes", function(req, res){
+	res.sendFile("/Recipe.html", {root: "./Recipes"})
+});
+server.get("/thebasics", function(req, res){
+	res.sendFile("/TheBasics.html", {root: "./The-Basics"})
+});
+
+// server.get("/:user", function(req, req){
+// 	res.sendFile()
+// });
