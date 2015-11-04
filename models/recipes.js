ecd689a63402f3807448a8d4a34c7e5ce22f6db7
Brent Leasure <brenton.leasure@hotmail.com>
2015-11-03 17:05:05 -0700

    some stuff

--- models/recipes.js
+++ models/recipes.js
@@ -20,5 +20,5 @@ var beerTypesSchema = mongoose.Schema({
 
 module.exports = {
 	userRecipe     : mongoose.model("userRecipe", recipeSchema),
-	beerTypes 	   : mongoose.model("beertypes", beerTypesSchema),
+	beerTypes 	   : mongoose.model("beerTypes", beerTypesSchema),
 }
