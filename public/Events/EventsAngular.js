af2801d13b24847ac1674ab76d7c3e8334ce55db
Brenton Leasure <brenton.leasure@hotmail.com>
2016-05-14 16:36:56 -0600

    some of the icons are showing up, but others are causing an 'OVER_QUERY_LIMIT' error.

--- public/Events/EventsAngular.js
+++ public/Events/EventsAngular.js
@@ -12,22 +12,44 @@ angular.module("indexModule")
 		$scope.getFestivals = function(){
 			$http.get("/getFestivals")
 			.then(function(returnData){
+				var geocoder = new google.maps.Geocoder();
 				$scope.events = returnData.data.events;
-				console.log($scope.events);
-				for(var i = 0; i < $scope.events.length; i++){
-					  	// var request = {
-					  	// 	location: centerOfMap,
-					  	// 	radius = 612000,
-					  	// 	keyword = $scope.events.location,
-					  	// }
+				$scope.geocodeAddress(geocoder, $scope.events);
+				// console.log($scope.events);
+				// for(var i = 0; i < $scope.events.length; i++){
+				// 	  	// var request = {
+				// 	  	// 	location: centerOfMap,
+				// 	  	// 	radius = 612000,
+				// 	  	// 	keyword = $scope.events.location,
+				// 	  	// }
 					
-				  	$scope.marker = new google.maps.Marker({
-						position: "Vail",
-						map: $scope.eventMap,
-						title: $scope.events.title,
-					});
-		  		}
+				//   	$scope.marker = new google.maps.Marker({
+				// 		position: "Vail",
+				// 		map: $scope.eventMap,
+				// 		title: $scope.events.title,
+				// 	});
+		  // 		}
 			})
 		}
-		
+		$scope.geocodeAddress = function(geocoder, events){
+			var count = 0;
+			console.log(events.length);
+			for(var i = 0; i < events.length; i++){
+				if(events[i].location != "N/A"){
+					geocoder.geocode({"address" : events[i].location}, function(results, status) {
+					    if (status === google.maps.GeocoderStatus.OK) {
+					      var marker = new google.maps.Marker({
+					        map: $scope.eventMap,
+					        position: results[0].geometry.location
+					      });
+					      count++;
+					      console.log(count);
+					    } else {
+					      console.log('Geocode was not successful for the following reason: ' + status);
+					    }
+
+					});
+				}
+			}
+		}
 }]);
