784a675f24ad1f1a74e5edd725f48117e8da3c20
Brenton Leasure <brenton.leasure@hotmail.com>
2016-06-07 23:04:33 -0600

    infowindows close properly now. Adding content that pops up later.

--- public/Events/EventsAngular.js
+++ public/Events/EventsAngular.js
@@ -1,6 +1,7 @@
 angular.module("indexModule")
 	.controller("EventsController", ["$scope", "$http", function($scope, $http){
 		var lastMarker;
+		var infoWindow;
 		$scope.initMap = function(){
 			var centerOfMap ={lat: 39.244785, lng: -105.511852};
 			var eventMap = new google.maps.Map(document.getElementById('eventMap'), {
@@ -25,15 +26,16 @@ angular.module("indexModule")
 			        position: new google.maps.LatLng(currentEvent.latitude, currentEvent.longitude),
 			    });
 				
-				marker.infoWindow = new google.maps.InfoWindow({
+				infoWindow = new google.maps.InfoWindow({
 	            	content: currentEvent.title,
 		        });
 		        google.maps.event.addListener(marker, 'click', function(){		    
-		            marker.infoWindow.open(map, marker);	
-		            // if(lastMarker != undefined){     
-		            // 	marker.infoWindow.close(map, lastMarker);		      
-		            // }
-		            // lastMarker = marker;     
+		            lastMarker = marker;    
+		            if(lastMarker){
+		            	infoWindow.close();		     
+		            }
+		            infoWindow.open(map, marker);	
+		          
 		        });
 		        return marker;
 	        }
